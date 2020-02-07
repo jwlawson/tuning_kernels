2f62514588ae6a898a532530cc709ccc54ac1459
John Lawson <john@codeplay.com>
2020-02-07 16:20:35 +0000

    Add feature augmentation to classifier

--- kernel_tree_classifier.py
+++ kernel_tree_classifier.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,6 +18,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 
 from sklearn.neighbors import KNeighborsClassifier as skNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 from load import *
 
@@ -272,6 +275,26 @@ def get_targets_for_given_configs(dataset, labels):
     return limited_norm.idxmax(axis=1)
 
 
+def augment_features(feats):
+
+    def get_feats(m, k, n, b):
+        return (m, k, n, b, m * n, b * m * n, k * n, k * m, m / k, n / k,
+                m * n / k)
+
+    feats['mn'] = feats['m'] * feats['n']
+    feats['bmn'] = feats['batch'] * feats['m'] * feats['n']
+    feats['kn'] = feats['k'] * feats['n']
+    feats['km'] = feats['k'] * feats['m']
+    feats['mpk'] = feats['m'] / feats['k']
+    feats['npk'] = feats['n'] / feats['k']
+    feats['mnpk'] = feats['m'] * feats['n'] / feats['k']
+
+    return feats, get_feats
+
+
+_, get_feats = augment_features(train_dataset.features)
+
+
 class GenModel():
     def __init__(self, model, classifier):
         labels = chosen_labels[model]
@@ -279,46 +302,45 @@ class GenModel():
         self.model = classifier.fit(train_dataset.features, x_labels)
 
     def get_config(self, m, k, n, batch):
-        return self.model.predict(np.asarray([(m, k, n, batch)]))
+        return self.model.predict(np.asarray([get_feats(m, k, n, batch)]))
 
 
 def get_classifier_performance(model):
     errors = [
-        test_dataset.normalized.iloc[i][model.get_config(
-            **test_dataset.features.iloc[i].to_dict())]
+        test_dataset.normalized.iloc[i][model.get_config(**test_dataset.features.iloc[i].to_dict())][0]
         for i in range(test_dataset.normalized.shape[0])
     ]
-    return geom_mean(errors)
+    return geom_mean(errors), errors
 
 
-def gen_tree_classifier():
-    return sktree.DecisionTreeClassifier(random_state=0)
-
-
-def gen_limited_tree_classifier():
-    return sktree.DecisionTreeClassifier(
-        random_state=0, max_depth=5, min_samples_split=4, min_samples_leaf=3)
-
-
-def gen_nearest_neighbor():
-    return skNeighborsClassifier(n_neighbors=7, metric='l2')
-
-
-CLASSIFIER_GEN = [
-    gen_tree_classifier,
-    gen_limited_tree_classifier,
-    gen_nearest_neighbor,
+CLASSIFIER_MOD = [DecisionTree]
+CLASSIFIER_CLS = [5, 6, 8, 15]
+CLASSIFIERS = [
+    ('DecisionTree', sktree.DecisionTreeClassifier(random_state=0)),
+    ('LimitedDecisionTree', sktree.DecisionTreeClassifier(
+        random_state=0,
+        max_features=4,
+        max_depth=6,
+        min_samples_split=4,
+        min_samples_leaf=3)),
+    ('1NearestNeighbor', skNeighborsClassifier(n_neighbors=1)),
+    ('3NearestNeighbor', skNeighborsClassifier(n_neighbors=3)),
+    ('7NearestNeighbor', skNeighborsClassifier(n_neighbors=7)),
+    ('LinearSVM', SVC(kernel="linear", C=0.025, cache_size=5000)),
+    ('RadialSVM', SVC(gamma=2, C=1, cache_size=5000)),
+    ('RandomForest', RandomForestClassifier(random_state=0)),
 ]
 
 
 def compare_classifiers():
-    for classifier in CLASSIFIER_GEN:
-        for n_c in N_CLASSES:
-            for model in MODELS:
-                classes = '{}{}'.format(model.cls_name, n_c)
-                m = GenModel(classes, classifier())
-                print(classes, classifier.__name__,
-                      get_classifier_performance(m))
+    for n_c in CLASSIFIER_CLS:
+        for model in CLASSIFIER_MOD:
+            classes = '{}{}'.format(model.cls_name, n_c)
+
+            for cls_name, classifier in CLASSIFIERS:
+                m = GenModel(classes, classifier)
+                error, errors = get_classifier_performance(m)
+                print(classes, cls_name, error)
 
 
 print("--- Classifier performance ---")
